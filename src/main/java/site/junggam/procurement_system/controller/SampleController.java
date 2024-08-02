a5466e0d8047d7ba56cfb45178bc04b2300145d4
LongTypeNo6 <longtypeno.6@gmail.com>
2024-08-02 13:19:41 +0900

    데이터베이스연결5
    방금꺼 쌍따옴표 추가했더니 안 됨 다시 삭제

---
+++ src/main/java/site/junggam/procurement_system/controller/SampleController.java
@@ -0,0 +1,17 @@
+﻿package site.junggam.procurement_system.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Log4j2
+@RequestMapping("/sample")
+public class SampleController {
+    
+    @RequestMapping("logintest1")
+    public void logintest1(){
+        log.info("테스트 하는 김에 로그포제이도 테스트");
+        System.out.println("테스트 하는 김에 시스아웃도 테스트");
+    }
+}
