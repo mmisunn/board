02e229b21ea8e3633f24bbfa2133c9aa7782acda
LongTypeNo6 <longtypeno.6@gmail.com>
2024-08-02 09:34:58 +0900

    초기 셋팅
    Project : Gradle - Groovy
    Language : Java
    Spring Boot : 3.3.2
    Project Metadata :
    	Group : site.junggam
    	Artifact : procurement-system
    	Name : procurement-system
    	Description : Demo project for Spring Boot
    	Package name : site.junggam.procurement-system
    	Packaging : Jar
    	Java : 17
    Dependencies : (총 7개)
    	Spring Boot DevTools
    	Lombok
    	Spring Web
    	Thymeleaf
    	Spring Data JPA
    	MariaDB Driver
    	Spring Security
    
    IntelliJ IDEA Community Edition 2024.1.3 사용

---
+++ src/main/java/site/junggam/procurement_system/ProcurementSystemApplication.java
@@ -0,0 +1,13 @@
+package site.junggam.procurement_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcurementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcurementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/junggam/procurement_system/ProcurementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package site.junggam.procurement_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcurementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
