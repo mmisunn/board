49eb7d1ea8457562ef352ae6b739a284c62f0322
LongTypeNo6 <longtypeno.6@gmail.com>
2024-08-06 18:01:20 +0900

    입출고내역 수정(임시)2
    내역에 있는 메소드가 아직 미완이라 오류 떠서 주석 처리 해둠, 승희(담당자) 전달 완료.
    어플리케이션 잘 들어감

--- src/test/java/site/junggam/procurement_system/repository/WarehousingRepositoryTests.java
+++ src/test/java/site/junggam/procurement_system/repository/WarehousingRepositoryTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class WarehousingRepositoryTests {
-    @Autowired
-    private WarehousingRepository
+//    @Autowired
+//    private WarehousingRepository
 
 
 
