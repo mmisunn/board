d1f402fb6ac4bb52a415c84ae016b59d47f90fa7
psh <dduppcms@gmail.com>
2024-08-08 17:00:09 +0900

    입출고내역 4
    생성(미완): 에러 수정과 엔티티 추가

--- src/main/java/site/junggam/procurement_system/service/WarehousingService.java
+++ src/main/java/site/junggam/procurement_system/service/WarehousingService.java
@@ -14,7 +14,7 @@ public interface WarehousingService {
     //출하명세서 파일 저장
     //이건 입고서아닌가?
     //아마 출하명세서는 또 다른 것 같은데..나중에 추가해야할듯
-    //..일단 지피티가 알려준대로 해본다..
+    //..일단 지피티가 알려준대로 해본다..다해,,,
     void savewarehousing(WarehousingDTO warehousingDTO);
 
     //dto-> entity변형
