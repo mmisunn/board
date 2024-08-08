c3c70b66e431c43e9c8f3bff45a142b193248063
LongTypeNo6 <longtypeno.6@gmail.com>
2024-08-08 17:17:42 +0900

    디펜던시 MapStruct 추가/발주서 작업중
    MapStruct이용해서 발주서 서비스 내용 바꿈

--- src/main/generated/site/junggam/procurement_system/mapper/PurchaseOrderMapperImpl.java
+++ src/main/generated/site/junggam/procurement_system/mapper/PurchaseOrderMapperImpl.java
@@ -11,7 +11,7 @@ import site.junggam.procurement_system.entity.TemMaterial;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-08-08T16:55:56+0900",
+    date = "2024-08-08T17:16:33+0900",
     comments = "version: 1.5.2.Final, compiler: javac, environment: Java 17.0.2 (Oracle Corporation)"
 )
 @Component
